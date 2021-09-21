c55fa88bb3bc10962eba2207fab890ae1c99c52f
João Pedrosa <jrcspp@hotmail.com>
2021-09-21 16:02:37 +0100

    all done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import contactsJSON from "./contacts.json";
+import React, { useState } from "react";
+
+
 
 function App() {
+  const [contacts, setContacts] = useState(contactsJSON.slice(0, 5));
+
+  const addRandomContact = () => {
+    
+    if (contactsJSON.length) {
+      const randomIndex = Math.floor(Math.random() * contactsJSON.length)
+      const newRandomContact = contactsJSON[randomIndex];
+      contactsJSON.splice(randomIndex, 1);
+      setContacts(contacts.concat(newRandomContact))
+    }
+  }
+
+  const sort = (key) => {
+    setContacts([...contacts.sort((a, b) => {
+      return a[key] > b[key] ? 1 : -1;
+    })])
+  }
+
+  const deleteContact = (id) => {
+    setContacts(contacts.filter((contact) => contact.id !== id)); 
+};
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {contactsJSON.length === 0 && "no more contacts"}
+
+      <h1>IronContacts</h1>
+
+      <button onClick={addRandomContact}>Add Random Contact</button>
+      <button onClick={() => sort("name")}>Sort By Name</button>
+      <button onClick={() => sort("popularity")}>Sort By Popularity</button>
+  
+
+      <table className="table">
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+          <th>Actions</th>
+        </tr>
+
+        {contacts.map((celeb, index) => {
+          return <tr key={index}>
+                    <td><img src={celeb.pictureUrl} width="100px" alt="fuck you" /></td>
+                    <td>{celeb.name}</td>
+                    <td>{celeb.popularity.toFixed(2)}</td>
+                    <td>{celeb.wonOscar ? "🏆" : ""}</td>
+                    <td>{celeb.wonEmmy ? "🏆" : ""}</td>
+                    <td><button onClick={() => deleteContact(celeb.id)}>Delete</button></td>
+                  </tr>
+        })}
+        
+      </table>
+      
     </div>
   );
 }
